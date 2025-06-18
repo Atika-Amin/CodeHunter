111b397f6bbecfb96275c1b787aa9988275a2d6c
Atika <aatika.amin98@gmail.com>
2025-06-18 13:49:44 +0600

    Add files via upload

---
+++ src/main/resources/assets/maps/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="32x32" tilewidth="32" tileheight="32" tilecount="390" columns="26">
+ <image source="F:/New folder (2)/Raven Fantasy - Pixel Art Tileset - Green Forest v2/All Tileset/32x32.png" width="832" height="480"/>
+</tileset>
