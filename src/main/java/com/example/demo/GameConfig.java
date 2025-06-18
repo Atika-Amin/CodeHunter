d138b12f9972ad935228a09d35732f446fbceae0
Atika <aatika.amin98@gmail.com>
2025-06-18 14:01:29 +0600

    Add files via upload

---
+++ src/main/java/com/example/demo/GameConfig.java
@@ -0,0 +1,10 @@
+// GameConfig.java
+package com.example.demo;
+
+public class GameConfig {
+    public static final int TILE_SIZE = 32;
+    public static final int VIEWPORT_WIDTH = 15;
+    public static final int VIEWPORT_HEIGHT = 10;
+    public static final int MAP_WIDTH = 40;
+    public static final int MAP_HEIGHT = 30;
+}
